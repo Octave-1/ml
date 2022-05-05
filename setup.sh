2d1e88d6a11432f47521ed4be2ad90ab5aca0a97
Tom Moran <tom.moran4@gmail.com>
2022-05-05 15:55:49 -0300

    WIP: get processed images

--- setup.sh
+++ setup.sh
@@ -17,6 +17,6 @@ envsubst  </root/.kaggle/kaggle_template.json >> /root/.kaggle/kaggle.json
 chmod 600 /root/.kaggle/kaggle.json
 gcloud auth activate-service-account dl-sa-495@nth-fiber-303315.iam.gserviceaccount.com --key-file=/root/.gcp/nth-fiber-303315-ee5378f17565.json --project=nth-fiber-303315
 mkdir -p datasets/retinopathy/
-gsutil -m cp -r "gs://ml-start-$1/train_images/" datasets/retinopathy/
-gsutil -m cp -r "gs://ml-start-$1/test_images/" datasets/retinopathy/
+gsutil -m cp -r "gs://ml-start-$1/train_images_processed/" datasets/retinopathy/
+# gsutil -m cp -r "gs://ml-start-$1/test_images_processed/" datasets/retinopathy/
 gsutil cp "gs://ml-start-$1/trainLabels.csv" datasets/retinopathy/
